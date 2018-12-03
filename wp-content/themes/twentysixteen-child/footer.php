7ee6a162620f9ad544fdd16e959bfe719935720f
Student <student@CVS-R212D002.nic.bc.ca>
2018-12-03 11:06:22 -0800

    remove wp info from footer, post meta data, add date to footer

---
+++ wp-content/themes/twentysixteen-child/footer.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+		</div><!-- .site-content -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<?php if ( has_nav_menu( 'primary' ) ) : ?>
+				<nav class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Primary Menu', 'twentysixteen' ); ?>">
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'primary',
+							'menu_class'     => 'primary-menu',
+						 ) );
+					?>
+				</nav><!-- .main-navigation -->
+			<?php endif; ?>
+
+			<?php if ( has_nav_menu( 'social' ) ) : ?>
+				<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentysixteen' ); ?>">
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'social',
+							'menu_class'     => 'social-links-menu',
+							'depth'          => 1,
+							'link_before'    => '<span class="screen-reader-text">',
+							'link_after'     => '</span>',
+						) );
+					?>
+				</nav><!-- .social-navigation -->
+			<?php endif; ?>
+
+			<div class="site-info">
+				<?php
+					/**
+					 * Fires before the twentysixteen footer text for footer customization.
+					 *
+					 * @since Twenty Sixteen 1.0
+					 */
+					do_action( 'twentysixteen_credits' );
+				?>
+				<!-- Add coppyright and post date. -->
+				<p>&copy; <?php echo get_the_date( $d = 'Y') ?></p>
+
+			</div><!-- .site-info -->
+		</footer><!-- .site-footer -->
+	</div><!-- .site-inner -->
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
